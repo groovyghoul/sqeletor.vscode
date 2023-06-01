1c93a53fbd5fc26db5a8d6964299409422458f30
Richard O'Neil <richard@bytemares.com>
2023-06-01 19:44:11 -0300

    refactor: script code to use same as proc

--- src/extension.ts
+++ src/extension.ts
@@ -3,49 +3,9 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
     let disposable1 = vscode.commands.registerCommand(
         "extension.createScript",
-        async () => {
-            const filename = await vscode.window.showInputBox({
-                prompt: "Enter script name (YYMMDDxxNN)",
-                value: getCurrentDate(),
-            });
-            if (filename) {
-                const ticket = await vscode.window.showInputBox({
-                    prompt: "Enter ticket",
-                });
-                if (ticket) {
-                    const description = await vscode.window.showInputBox({
-                        prompt: "Enter description",
-                    });
-
-                    // Generate the templated text
-                    const template = `set term !;
-                
-                
-
-set term ;!
-                
-insert into applied_scripts (name, description, script_date)
-    values ('${filename.toLowerCase()}', '${ticket.toUpperCase()} ${description}', '${getAppliedScriptsDate()}');`;
-
-                    // Create a new text document with the generated text
-                    const doc = await vscode.workspace.openTextDocument({
-                        language: "sql",
-                        content: template,
-                    });
-                    const editor = await vscode.window.showTextDocument(doc);
-
-                    const newPosition = new vscode.Position(2, 0);
-                    const newSelection = new vscode.Selection(
-                        newPosition,
-                        newPosition
-                    );
-
-                    editor.selection = newSelection;
-                }
-            }
-        }
+        createScript
     );
-
+    
     let disposable2 = vscode.commands.registerCommand(
         "extension.createStoredProcedure",
         createStoredProcedure
@@ -55,6 +15,72 @@ insert into applied_scripts (name, description, script_date)
 }
 
 export function deactivate() {}
+
+function createScript() {
+    const promptInputs = [
+        { prompt: "Enter script name (YYMMDDxxNN)", value: getCurrentDate() },
+        { prompt: "Enter ticket", value: "" },
+        { prompt: "Enter description", value: "" },
+    ];
+
+    let currentPrompt = 0;
+
+    const handleInput = (input: string | undefined) => {
+        if (input) {
+            promptInputs[currentPrompt].value = input;
+            currentPrompt++;
+
+            if (currentPrompt < promptInputs.length) {
+                vscode.window
+                    .showInputBox({
+                        prompt: promptInputs[currentPrompt].prompt,
+                        value: promptInputs[currentPrompt].value,
+                    })
+                    .then(handleInput);
+            } else {
+                // All inputs collected, create the templates
+                const fileName = promptInputs[0].value.toLowerCase();
+                const ticketNumber = promptInputs[1].value.toUpperCase();
+                const description = promptInputs[2].value;
+
+                    // Generate the templated text
+                    const template = `set term !;
+                
+                
+
+set term ;!
+                
+insert into applied_scripts (name, description, script_date)
+    values ('${fileName.toLowerCase()}', '${ticketNumber.toUpperCase()} ${description}', '${getAppliedScriptsDate()}');`;
+
+                const firstFileVirtualUri = vscode.Uri.parse(
+                    `untitled:${fileName}.sql`
+                );
+                vscode.workspace
+                    .openTextDocument(firstFileVirtualUri)
+                    .then((doc) => {
+                        const edit = new vscode.WorkspaceEdit();
+                        edit.insert(
+                            firstFileVirtualUri,
+                            new vscode.Position(0, 0),
+                            template
+                        );
+                        vscode.workspace.applyEdit(edit).then(() => {
+                            vscode.window.showTextDocument(doc, {
+                                preview: false,
+                                viewColumn: vscode.ViewColumn.Active,
+                                preserveFocus: true,
+                            });
+                        });
+                    });
+            }
+        }
+    };
+
+    vscode.window
+        .showInputBox({ prompt: promptInputs[currentPrompt].prompt, value: promptInputs[currentPrompt].value })
+        .then(handleInput);
+}
 
 function createStoredProcedure() {
     const promptInputs = [
