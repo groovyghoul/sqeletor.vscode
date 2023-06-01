459b23029b46f9c325cd964767c282a05c962ef6
Richard O'Neil <richard@bytemares.com>
2023-06-01 20:08:23 -0300

    fix: set proper focus on new script tab

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
         "extension.createScript",
         createScript
     );
-    
+
     let disposable2 = vscode.commands.registerCommand(
         "extension.createStoredProcedure",
         createStoredProcedure
@@ -43,8 +43,8 @@ function createScript() {
                 const ticketNumber = promptInputs[1].value.toUpperCase();
                 const description = promptInputs[2].value;
 
-                    // Generate the templated text
-                    const template = `set term !;
+                // Generate the templated text
+                const template = `set term !;
                 
                 
 
@@ -65,12 +65,36 @@ insert into applied_scripts (name, description, script_date)
                             new vscode.Position(0, 0),
                             template
                         );
+
                         vscode.workspace.applyEdit(edit).then(() => {
-                            vscode.window.showTextDocument(doc, {
-                                preview: false,
-                                viewColumn: vscode.ViewColumn.Active,
-                                preserveFocus: true,
-                            });
+                            vscode.window
+                                .showTextDocument(doc, {
+                                    preview: false,
+                                    viewColumn: vscode.ViewColumn.Active,
+                                    preserveFocus: true,
+                                })
+                                .then((editor) => {
+                                    const newPosition = new vscode.Position(
+                                        2,
+                                        0
+                                    );
+                                    const newSelection = new vscode.Selection(
+                                        newPosition,
+                                        newPosition
+                                    );
+                                    editor.selection = newSelection;
+
+                                    editor.revealRange(
+                                        new vscode.Range(
+                                            newPosition,
+                                            newPosition
+                                        ),
+                                        vscode.TextEditorRevealType.InCenter
+                                    );
+                                    vscode.commands.executeCommand(
+                                        "workbench.action.focusActiveEditorGroup"
+                                    );
+                                });
                         });
                     });
             }
@@ -78,7 +102,10 @@ insert into applied_scripts (name, description, script_date)
     };
 
     vscode.window
-        .showInputBox({ prompt: promptInputs[currentPrompt].prompt, value: promptInputs[currentPrompt].value })
+        .showInputBox({
+            prompt: promptInputs[currentPrompt].prompt,
+            value: promptInputs[currentPrompt].value,
+        })
         .then(handleInput);
 }
 
@@ -192,7 +219,10 @@ set term ;!
     };
 
     vscode.window
-        .showInputBox({ prompt: promptInputs[currentPrompt].prompt, value: promptInputs[currentPrompt].value })
+        .showInputBox({
+            prompt: promptInputs[currentPrompt].prompt,
+            value: promptInputs[currentPrompt].value,
+        })
         .then(handleInput);
 }
 
